146be5beb76c38485384a96ec0d5584848cd3a68
Faria Mollika <149615629+MollikaFaria06@users.noreply.github.com>
2025-09-19 22:16:52 +0600

    added 2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Suspense } from 'react'
 import Countries from '../components/Countries/Countries'
 import './App.css'
+
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+   .then(res => res.json())
 
 function App() {
   
   return (
     <>
-      <Countries></Countries>
+      <Suspense fallback={<p>Nadir Vai Loading..........</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
       
     </>
   )
