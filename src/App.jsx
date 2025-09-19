e35661e2d8bc73ff33c67df0d19abcd9278dbcd2
Faria Mollika <149615629+MollikaFaria06@users.noreply.github.com>
2025-09-19 14:20:25 +0600

    added first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Countries from '../components/Countries/Countries'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
